bd5da54fcba0a5de08fdcde776352ce23b0a46b5
ArthurWalker <phuc.phamhuu2@dcu.ie>
2019-05-03 17:14:22 +0100

    Get used to props

--- review-reactjs/src/components/Main.js
+++ review-reactjs/src/components/Main.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReviewVariable from './sub_components/ReviewVariable';
 import Prop1 from './sub_components/Prop1';
 import Prop2 from './sub_components/Prop2';
+import Array from './sub_components/Array';
 
 function Main(){
     return (
         <div>
+            {/**Lesson 1 */}
             <ReviewVariable/>
+            {/**Lesson 2 */}
             <Prop1/>
+            {/**Lesson 3 */}
             <Prop2/>
+            {/**Lesson 4 */}
+            <Array/>
         </div>
     )
 }
